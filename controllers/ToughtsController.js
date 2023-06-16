65ffb4042e4a1bbaa0e8f841497066d2492ace28
Kelvison Carlos Vieira <kelvison.vieira@live.com>
2023-06-15 21:33:12 -0300

    update toughs

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -14,13 +14,17 @@ module.exports = class ToughtsController{
         include: Tought,
          plain: true,
        })
-       if(!user){
+       if(!user){ 
         res.redirect('/login')
        }
-
+     
         const toughts= user.Toughts.map((result)=> result.dataValues)
-        console.log(toughts)
-        res.render('toughts/dashboard',{toughts})
+        
+        let emptyToughts = false
+         if(toughts.length ==0){
+          emptyToughts= true
+         }
+        res.render('toughts/dashboard',{toughts,emptyToughts})
     }
     static create(req,res){
         res.render('toughts/create')
@@ -40,5 +44,30 @@ module.exports = class ToughtsController{
       } catch(error){
         console.log(error)
       }
+    }
+    static async removeTought(req,res){ 
+    const id= req.params.id
+     const UserId= req.session.userid
+    await Tought.destroy({where:{ id:id, UserId: UserId}})
+    req.flash('message','Pensamento removido com sucesso!')     
+    req.session.save(()=>{
+      res.redirect('/toughts/dashboard')
+    })
+    
+    }
+    static async edit(req,res){
+      const id= req.params.id
+      const tought = await Tought.findOne({where:{id:id}, raw:true})
+      res.render('toughts/edit',{tought})
+    }
+    static async editPost(req,res){
+      const id=req.body.id
+      const tought={
+        title:req.body.title,
+      }
+      
+      await Tought.update(tought,{where:{id:id}})
+      req.flash('message',)
+      res.redirect('/toughts/dashboard')
     }
 }
--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -9,7 +9,9 @@ const checkAuth = require('../helpers/auth').checkAuth
 router.get('/',ToughtsController.showToughts)
 
 router.get('/add', checkAuth, ToughtsController.create)
+router.get('/edit/:id', checkAuth, ToughtsController.edit)
+router.post('/edit', checkAuth, ToughtsController.editPost)
 router.post('/add', checkAuth, ToughtsController.createPost)
 router.get('/dashboard', checkAuth, ToughtsController.dashboard)
-
+router.get('/remove/:id',ToughtsController.removeTought)
 module.exports = router
